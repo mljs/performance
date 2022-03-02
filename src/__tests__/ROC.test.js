b2ac242f14c4e6b539f25ae613b7a2586a5e8e67
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-03-02 18:51:12 +0100

    refactor: cleanup

--- src/__tests__/ROC.test.js
+++ src/__tests__/ROC.test.js
@@ -1,7 +1,7 @@
 import Performance from '..';
 
-describe('ROC curve', function () {
-  it('distance matrix', function () {
+describe('ROC curve', () => {
+  it('distance matrix', () => {
     const pred = [
       [0.0, 0.4, 0.8, 0.9, 0.1],
       [0.4, 0.0, 0.3, 0.5, 0.2],
@@ -18,9 +18,9 @@ describe('ROC curve', function () {
     ];
 
     const perf = new Performance(pred, target);
-    expect(perf.nSamples).toStrictEqual(10);
-    expect(perf.nPos).toStrictEqual(4);
-    expect(perf.nNeg).toStrictEqual(6);
+    expect(perf.nSamples).toBe(10);
+    expect(perf.nPos).toBe(4);
+    expect(perf.nNeg).toBe(6);
     expect(perf.cutoffs).toStrictEqual([
       Number.MIN_VALUE,
       0.1,
@@ -40,9 +40,9 @@ describe('ROC curve', function () {
     expect(perf.getAURC()).toBeCloseTo(0.958, 3);
 
     const fpr = perf.getMeasure('fpr');
-    expect(fpr[0]).toStrictEqual(0);
-    expect(fpr[6]).toStrictEqual(0.5);
-    expect(fpr[9]).toStrictEqual(1);
+    expect(fpr[0]).toBe(0);
+    expect(fpr[6]).toBe(0.5);
+    expect(fpr[9]).toBe(1);
 
     const tpr = perf.getMeasure('tpr');
     expect(tpr).toStrictEqual([0, 0.25, 0.75, 0.75, 1, 1, 1, 1, 1, 1]);
